36d7b15a34a1e5607f7c8b44e2a6b6081914b612
ArtyIF <artyomisflash@mail.ru>
2022-07-30 07:17:50 +0300

    Fixed potfile update script

--- po/update_potfile.sh
+++ po/update_potfile.sh
@@ -4,11 +4,7 @@ xgettext -f "$po_dir"/POTFILES -o "$po_dir"/AdwCustomizer.pot --add-comments=Tra
 sed -i "s/SOME DESCRIPTIVE TITLE./Adwaita Manager POT file/" "$po_dir"/AdwCustomizer.pot
 sed -i "s/YEAR THE PACKAGE'S COPYRIGHT HOLDER/$(date +%Y) Adwaita Manager Team/" "$po_dir"/AdwCustomizer.pot
 sed -i "s@same license as the PACKAGE package.@MIT/X11 license.@" "$po_dir"/AdwCustomizer.pot
-sed -i "s/FIRST AUTHOR <EMAIL@ADDRESS>, YEAR./Artyom Fomin <artyomisflash@mail.ru>, $(date +%Y)./" "$po_dir"/AdwCustomizer.pot
+sed -i "s/FIRST AUTHOR <EMAIL@ADDRESS>, YEAR./Adwaita Manager Team, $(date +%Y)./" "$po_dir"/AdwCustomizer.pot
 
 regex="$po_dir/([a-zA-Z_]*).po"
-po_files=$(find "$po_dir" -type f -name "*.po")
-if [[ $po_files =~ $regex ]]
-then
-  echo "${BASH_REMATCH[1]}" > "$po_dir"/LINGUAS
-fi
+find "$po_dir" -type f -name "*.po" | sed -rn "s:$regex:\1:p" > "$po_dir/LINGUAS"
