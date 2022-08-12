e13af9f723333ad3b2311b4cf9393f2d2a9d37d8
0xMRTT <0xMRTT@tuta.io>
2022-08-12 11:28:57 +0200

    update license

--- po/update_potfile.sh
+++ po/update_potfile.sh
@@ -3,7 +3,7 @@ po_dir=$(dirname "$(realpath "$0")")
 xgettext -f "$po_dir"/POTFILES -o "$po_dir"/AdwCustomizer.pot --add-comments=Translators --keyword=_ --keyword=C_1c,2 --from-code=UTF-8
 sed -i "s/SOME DESCRIPTIVE TITLE./Adwaita Manager POT file/" "$po_dir"/AdwCustomizer.pot
 sed -i "s/YEAR THE PACKAGE'S COPYRIGHT HOLDER/$(date +%Y) Adwaita Manager Team/" "$po_dir"/AdwCustomizer.pot
-sed -i "s@same license as the PACKAGE package.@MIT/X11 license.@" "$po_dir"/AdwCustomizer.pot
+sed -i "s@same license as the PACKAGE package.@GNU GPLv3 license.@" "$po_dir"/AdwCustomizer.pot
 sed -i "s/FIRST AUTHOR <EMAIL@ADDRESS>, YEAR./Adwaita Manager Team, $(date +%Y)./" "$po_dir"/AdwCustomizer.pot
 
 regex="$po_dir/([a-zA-Z_]*).po"
