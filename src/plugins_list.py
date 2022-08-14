c4ff817f0367d401afdc6f6e129202015c61a43a
0xMRTT <0xMRTT@tuta.io>
2022-08-14 11:50:12 +0200

    fix: import error

--- src/plugins_list.py
+++ src/plugins_list.py
@@ -16,7 +16,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-from .plugins.gtk4 import AdwcustomizerGtk4Plugin
 import os
 from pathlib import Path
 import importlib
