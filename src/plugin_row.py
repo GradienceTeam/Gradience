7f7c31cfcfa05c082712e358d19e488e4f23f371
0xMRTT <0xMRTT@tuta.io>
2022-08-19 14:23:25 +0200

    feat: add plugin row

---
+++ src/plugin_row.py
@@ -0,0 +1,17 @@
+
+from gi.repository import Gtk, Gdk, Adw
+
+from .constants import rootdir
+
+
+@Gtk.Template(resource_path=f"{rootdir}/ui/plugin_row.ui")
+class AdwcustomizerPluginRow(Adw.ActionRow):
+    __gtype_name__ = "AdwcustomizerPluginRow"
+
+    def __init__(self, name, repo, **kwargs):
+        super().__init__(**kwargs)
+
+        self.name = name
+        self.set_name(name)
+        self.set_title(name)
+        self.set_subtitle("@" + repo)
