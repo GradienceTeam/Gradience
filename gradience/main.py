3d30eba01108eb1d75fb38e8fa35a4704b605476
0xMRTT <0xMRTT@tuta.io>
2022-09-30 18:18:27 +0200

    fix: typo error

--- gradience/main.py
+++ gradience/main.py
@@ -986,27 +986,27 @@ class GradienceApplication(Adw.Application):
             version=version,
             release_notes_version=rel_ver,
             release_notes=_(
-                """
-                <ul>
-                <li>Add AdwViewSwitcher in the header bar.</li>
-                <li>Move CSS to the "Advanced" tab</li>
-                <li>Move the rest to the "colors" tab</li>
-                <li>Add Monet tab which generates a theme from a background</li>
-                <li>Add disk saved and disk unsaved icon in the header bar</li>
-                <li>Update about dialog</li>
-                <li>Change license to GNU GPLv3</li>
-                <li>Begin plugin support</li>
-                <li>Move preset selector to a drop-down called palette (icon palette)</li>
-                <li>Add ability to apply the theme onlyfor dark theme or oy for light theme</li>
-                <li>Automaticly use Adwaita-dark preset if the user prefered scheme is dark.</li>
-                <li>Added Flatpak CI build</li>
-                <li>Added issue template for bug and feature request </li>
-                <li>`Main` branch is now protected by GitHub branch protection. The development is done on `next` branch</li>
-                </ul>
-                """
+"""
+<ul>
+<li>Add AdwViewSwitcher in the header bar.</li>
+<li>Move CSS to the "Advanced" tab</li>
+<li>Move the rest to the "colors" tab</li>
+<li>Add Monet tab which generates a theme from a background</li>
+<li>Add disk saved and disk unsaved icon in the header bar</li>
+<li>Update about dialog</li>
+<li>Change license to GNU GPLv3</li>
+<li>Begin plugin support</li>
+<li>Move preset selector to a drop-down called palette (icon palette)</li>
+<li>Add ability to apply the theme only for dark theme or oy for light theme</li>
+<li>Automaticly use Adwaita-dark preset if the user prefered scheme is dark.</li>
+<li>Added Flatpak CI build</li>
+<li>Added issue template for bug and feature request </li>
+<li>`Main` branch is now protected by GitHub branch protection. The development is done on `next` branch</li>
+</ul>
+"""
             ),
             comments=_(
-                """
+"""
 Gradience is a tool for customizing Libadwaita applications and the adw-gtk3 \
 theme.
 With Gradience you can:
@@ -1018,7 +1018,7 @@ With Gradience you can:
 - Extend functionality using plugins
 
 This app is written in Python and uses GTK 4 and libadwaita.
-            """
+"""
             ),
         )
         about.add_credit_section(
